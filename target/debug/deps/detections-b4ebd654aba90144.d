359a416dd48050d077cc2531fd3bf1ad6f7291c9
Humair <141406804+HumairKashan@users.noreply.github.com>
2026-01-02 21:56:49 +0400

    Fix: Move tests folder to project root (out of src/)

---
+++ target/debug/deps/detections-b4ebd654aba90144.d
@@ -0,0 +1,5 @@
+C:\Users\roden\Desktop\Rust\Rust side projects\Sentinel\target\debug\deps\detections-b4ebd654aba90144.d: tests\detections.rs
+
+C:\Users\roden\Desktop\Rust\Rust side projects\Sentinel\target\debug\deps\libdetections-b4ebd654aba90144.rmeta: tests\detections.rs
+
+tests\detections.rs:
